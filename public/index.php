67f348815131dc6333bdf9d714bc876e9659176b
Moritz <2nd@moritzhuber.de>
2022-09-21 13:53:22 +0200

    new

--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$exp = explode(DIRECTORY_SEPARATOR, getcwd());
-array_pop($exp);
-define("ROOT", implode(DIRECTORY_SEPARATOR, $exp));
-
-require ROOT . '/framework/autoloader.php';
-
-view('index');

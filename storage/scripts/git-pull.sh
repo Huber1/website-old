f26dde5ce24e29eea38a1f3ba2b1c371beb59b54
Moritz <2nd@moritzhuber.de>
2022-09-26 13:43:17 +0200

    pull

--- storage/scripts/git-pull.sh
+++ storage/scripts/git-pull.sh
@@ -1,5 +1,10 @@
 cd "$1" || exit 0
 
-git pull
+git fetch
+git reset --hard HEAD
+git merge '@u'
+export COMPOSER_HOME="/usr/www/users/ulrichyb/public-moritz/composer"
 composer install --prefer-dist --no-progress
 ./tailwindcss -i ./src/style.css -o ./public/css/style.css --minify
+
+echo "pulled"

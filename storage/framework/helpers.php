b5f0eba4341de5ac5ff4dcfa72833366742e552f
Moritz <2nd@moritzhuber.de>
2022-09-26 14:16:10 +0200

    added status codes to view()

--- storage/framework/helpers.php
+++ storage/framework/helpers.php
@@ -6,7 +6,7 @@ if (!function_exists('view')) {
      * @param array $data
      * @return string
      */
-    function view(string $view, array $data = []): string
+    function view(string $view, array $data = [], $status = 200): string
     {
         $loader = new \Twig\Loader\FilesystemLoader(ROOT . '/src/views');
         $twig = new \Twig\Environment($loader, [
@@ -16,11 +16,12 @@ if (!function_exists('view')) {
         if (!str_ends_with($view, '.twig'))
             $view .= '.twig';
 
+        http_response_code($status);
+
         try {
             return $twig->render($view, $data);
         } catch (\Twig\Error\LoaderError|\Twig\Error\RuntimeError|\Twig\Error\SyntaxError $e) {
-            header("HTTP/1.0 404 Not Found");
-            return view('404');
+            return view('404', status: 404);
         }
     }
 }

898ecc3464f9f97b7a4da886df7b5bf8e82c6ee1
Moritz <2nd@moritzhuber.de>
2022-09-26 13:52:26 +0200

    Hallo Welt

--- storage/scripts/git-pull.sh
+++ storage/scripts/git-pull.sh
@@ -2,6 +2,7 @@ cd "$1" || exit 0
 
 git pull
 export COMPOSER_HOME="/usr/www/users/ulrichyb/public-moritz/composer"
+alias composer="php composer.phar"
 composer install --prefer-dist --no-progress
 ./tailwindcss -i ./src/style.css -o ./public/css/style.css --minify
 

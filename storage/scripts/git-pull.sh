b6184bf23a34372ac2345282d399f3dcc6d6da20
Moritz Huber <2nd@moritzhuber.de>
2022-11-30 23:16:24 +0100

    erst cache löschen, dann update

--- storage/scripts/git-pull.sh
+++ storage/scripts/git-pull.sh
@@ -1,10 +1,10 @@
 cd "$1" || exit 0
 
+rm -rf storage/cache
 git pull
 export COMPOSER_HOME="$1/composer"
 alias composer="php composer.phar"
 composer install --prefer-dist --no-progress
 ./tailwindcss -i ./src/style.css -o ./public/css/style.css --minify
-rm -rf storage/cache
 
 exit 1
